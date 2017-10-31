09aabfacf1baaffd5b7219ca6531d54a554e5310
guwenjia <1023478561@qq.com>
2017-10-31 17:19:50 +0800

    更新了web部分的内容

---
+++ 1/16-web/01-basic-server.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+var http = require('http');
+
+http.createServer(function(req,res){
+  var body = 'hello world!';
+
+  res.statusCode = 404;
+  res.setHeader('Content-length',Buffer.byteLength(body));
+  res.setHeader('Content-type','text/plain');
+
+  console.log(req.headers);
+  req.pipe(process.stdout);
+  console.log('');
+
+
+  res.end(body);
+}).listen(8080);
---
+++ 1/16-web/02-static-http-sever.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+var http=require('http');
+var fs=require('fs');
+
+var root = __dirname +'/' +(process.argv[2] || 'www');
+
+http.createServer(function(req,res){
+  var url = 'http://'+req.headers.host + req.url;
+
+  console.log('URL:',url);
+  console.log(req.headers);
+  console.log('');
+
+  var fileName= root +req.url;
+  fs.createReadStream(fileName).pipe(res);
+}).listen(8080);
+
